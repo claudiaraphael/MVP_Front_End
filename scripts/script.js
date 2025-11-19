24d45d23274c2f75247cff4b54d6ab87459685b4
main <claudia.raphael73@gmail.com>
2025-11-19 00:34:36 -0300

    layout algorithm project, align items center, approve navbar

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,9 @@
 
 // entrada do usuario
 const produto = document.getElementById("user-input");
+
+// 1 - pega o produto
+// 2 - pega os selos de sustentabilidaade da info da endpoint da api
+// 3 - compara com os verdadeiros selos dos produtos
+// 4 - da o score de sustentabilidade
+// 5 - manda o score pro servidor do banco de dados
