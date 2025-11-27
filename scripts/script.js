02bbcdadd9dd0a0a6f5207050238853a039d951c
main <claudia.raphael73@gmail.com>
2025-11-27 16:45:55 -0300

    trying to add a placeholder bg , adding form functions in javascript, figuring out how to add the bg0 image in assets

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,15 @@ const produto = document.getElementById("user-input");
 // 3 - compara com os verdadeiros selos dos produtos
 // 4 - da o score de sustentabilidade
 // 5 - manda o score pro servidor do banco de dados
+
+/* Function to create a new product */
+const postItem = async (inputProduct, inputBarcode, InputComment) => {
+    const formData = new FormData();
+    formData.append('name', inputProduct);
+    formData.append('barcode', inputProduct);
+    formData.append('comment', inputProduct);
+
+
+
+
+}
